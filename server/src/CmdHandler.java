5a3570508de43ecd9ddd3191c378f67a63855857
Uros Mihajlovic <mihajlovic.uros.96@gmail.com>
2018-07-31 22:56:45 +1200

    Fixed a flow in a command format checker

--- server/src/CmdHandler.java
+++ server/src/CmdHandler.java
@@ -6,15 +6,19 @@ public class CmdHandler {
 
     public String handleCommand(String cmd) {
 
+        // First check if the command is at least 5 characters long and if there is a space after the 4 letter command
+        // e.g. 'USER ' is valid, but 'USER' or 'USERx' will return error.
+        if(cmd.length() < 5 || cmd.charAt(4) != ' ') {
+            return "-invalid command";
+        } // Add an empty character if user specified 4 char command but no arguments and let argument checkers take care of it.
+        else if(cmd.length() < 6) {
+            cmd += " ";
+        }
+
         // Split user command so we can get the 4 character command and argument
         // We know that the command always starts with 4 characters and rest is arguments.
         String cdm_id = cmd.substring(0, Math.min(cmd.length(), 4));
         String arg = cmd.substring(5,cmd.length());
-
-        // Check for the validity of the argument
-        if(cmd.charAt(4) != ' ') {
-            return "-invalid command";
-        }
 
         switch(cdm_id) {
             case "USER":
