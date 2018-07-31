acc4f62a5801c2d887846a71299c72b6f24821be
Uros Mihajlovic <mihajlovic.uros.96@gmail.com>
2018-07-31 22:41:15 +1200

    Added command validator

--- server/src/CmdHandler.java
+++ server/src/CmdHandler.java
@@ -6,9 +6,19 @@ public class CmdHandler {
 
     public String handleCommand(String cmd) {
 
-        switch(cmd) {
+        // Split user command so we can get the 4 character command and argument
+        // We know that the command always starts with 4 characters and rest is arguments.
+        String cdm_id = cmd.substring(0, Math.min(cmd.length(), 4));
+        String arg = cmd.substring(5,cmd.length());
+
+        // Check for the validity of the argument
+        if(cmd.charAt(4) != ' ') {
+            return "-invalid command";
+        }
+
+        switch(cdm_id) {
             case "USER":
-                return authoriseUser(cmd);
+                return authoriseUser(arg);
             case "ACCT":
                 return "acct";
             case "PASS":
@@ -21,6 +31,6 @@ public class CmdHandler {
     }
 
     private String authoriseUser(String user) {
-        return "-Invalid user-id, try again";
+        return user;
     }
 }
